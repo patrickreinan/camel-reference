48423c679701aa557557809af0b8071dd5d52a86
patrickreinan <patrickreinan@gmail.com>
2023-08-14 23:30:41 -0300

    resequencer has been added

---
+++ resequencer/Resequencer.java
@@ -0,0 +1,21 @@
+// camel-k: language=java
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+
+public class Resequencer extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        // Write your routes here, for example:
+       // from("timer:java?period={{time:1000}}")
+        from("timer:java?period={{time:10000}}")
+            .setBody(constant("4,2,5,3,1"))
+            .split()
+            .tokenize(",")
+            .resequence(body())
+            .log("${body}");
+            
+    }
+}
